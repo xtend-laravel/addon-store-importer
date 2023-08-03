ad3c9d4b631bc887f2ac11407a5b0e45db5c2bb1
Adam Lee <adam@codelabx.ltd>
2023-08-03 16:17:35 +0100

    Refactor images + new method to assign images to variant by color

--- src/Base/Processors/Catalogue/ProductImages.php
+++ src/Base/Processors/Catalogue/ProductImages.php
@@ -4,7 +4,9 @@ namespace XtendLunar\Addons\StoreImporter\Base\Processors\Catalogue;
 
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
+use Lunar\Models\ProductVariant;
 use XtendLunar\Addons\StoreImporter\Base\Processors\Catalogue\Concerns\InteractsWithProductModel;
 use XtendLunar\Addons\StoreImporter\Base\Processors\Processor;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
@@ -14,8 +16,11 @@ class ProductImages extends Processor
 {
     use InteractsWithProductModel;
 
+    protected Collection $images;
+
     public function process(Collection $product, StoreImporterResourceModel $resourceModel): void
     {
+        $this->images = collect();
         $this->setProductModel($product->get('productModel'));
 
         $images = collect($product->get('images'));
@@ -23,20 +28,55 @@ class ProductImages extends Processor
             return;
         }
 
-        $images->each(
+        $images->collapse()->each(
             fn ($image, $key) => $this->saveImage($image, $key),
         );
+
+        if ($this->images->isNotEmpty()) {
+            // @todo Create separate job for variant images
+            $this->variantImages($product);
+        }
+    }
+
+    protected function variantImages(Collection $product): void
+    {
+        $this->getProductModel()->variants->each(function (ProductVariant $variant) use ($product) {
+            $colorOption = $variant->values->first(
+                fn ($value) => $value->option->handle === 'color',
+            )?->translate('name');
+
+            $matchedOption = collect($product->get('options'))
+                ->first(fn ($option) => $option['color'][0]['name'] === $colorOption);
+
+            if (! $colorOption || ! $matchedOption) {
+                return;
+            }
+
+            $imageIds = $this->images
+                ->filter(fn ($image, $imageId) => in_array($image, $matchedOption['images']))
+                ->keys();
+
+            foreach ($imageIds as $key => $imageId) {
+                $imagesToSync[$imageId] = [
+                    'primary' => $key === 0,
+                    'position' => $key+1,
+                ];
+            }
+
+            $variant->images()->sync($imagesToSync);
+        });
     }
 
     protected function saveImage(string $image, int $key): void
     {
-        if (!Http::get($image)->ok()) {
-			return;
-		}
-
         if ($this->imageExists($image)) {
             return;
         }
+
+        if (!Http::get($image)->ok()) {
+            Log::driver('slack')->warning("Image {$image} could not be downloaded.");
+			return;
+		}
 
         $media = $this->getProductModel()
             ->addMediaFromUrl($image)
@@ -49,10 +89,19 @@ class ProductImages extends Processor
     protected function imageExists(string $image): bool
     {
         // @todo Allow to replace image on force update?
-        $filename = basename(parse_url($image, PHP_URL_PATH));
+        $imageFilename = basename(parse_url($image, PHP_URL_PATH));
 
-        return $this->getProductModel()->getMedia('products')->map(
-            fn (Media $media) => Str::of($media->file_name)->beforeLast('.')
-        )->contains($filename);
+        $mediaCollection = $this->getProductModel()->getMedia('images')->mapWithKeys(
+            fn (Media $media) => [
+                $media->id => Str::of($media->file_name)->beforeLast('.')->value(),
+            ],
+        );
+
+        $matchedMediaId = $mediaCollection->filter(
+            fn ($filename) => $filename === $imageFilename,
+        )->keys()->first();
+
+        $this->images->put($matchedMediaId, $image);
+        return !!$matchedMediaId;
     }
 }
